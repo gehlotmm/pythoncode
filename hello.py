41ca5117b2d995cf180ab287ec633169187b5985
gehlotmm <48724433+gehlotmm@users.noreply.github.com>
2019-03-19 20:25:15 +0530

    changed something

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 print("helllo world")
 
 name  = input("Enter your name : ")
-
+print("python is simple, git is so simple")
 print("Hello user : ",name)
-
-
+print("ohh it is awesome")
 print("bye world")
